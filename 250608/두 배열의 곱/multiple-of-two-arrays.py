bddcd78ea25645f971707925005000c454b4e672
Sangrix <min393917@gmail.com>
2025-06-08 15:59:56 +0900

    Title: 두 배열의 곱, Time: 71ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250608/두 배열의 곱/multiple-of-two-arrays.py
+++ 250608/두 배열의 곱/multiple-of-two-arrays.py
@@ -1,31 +1,19 @@
-arr_first = [
-    [0 for _ in range(3)] for _ in range(3)
-]
+arr_first = []
+for _ in range(3):
+    arr = list(map(int,input().split()))
+    arr_first.append(arr)
 
-arr_second = [
-    [0 for _ in range(3)] for _ in range(3)
-]
+arr_second = []
+for _ in range(3):
+    arr = list(map(int,input().split()))
+    arr_second.append(arr)
 
 arr_multi = [
      [0 for _ in range(3)] for _ in range(3)
 ]
 
-num_first = 1
-num_second =2
-
 for i in range(3):
     for j in range(3):
-        arr_first[i][j] = num_first
-        num_first += 1
-
-
-for i in range(3):
-    for j in range(3):
-        arr_second[i][j] = num_second
-        num_second += 1
-
-for i in range(3):
-    for j in range(3):
-        arr_multi[i][j] = (arr_first[i][j] * arr_second[i][j])
+        arr_multi[i][j] = arr_first[i][j] * arr_second[i][j]
         print(arr_multi[i][j],end = " ")
     print()
